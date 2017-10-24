622d1a59ce9817e978970bb65c355df59f610526
Helder <vieiraa360@gmail.com>
2017-10-24 15:35:26 +0100

    update

--- js/app.js
+++ js/app.js
@@ -19,8 +19,40 @@ angular.module('ElsaArtur').config(function($locationProvider, $routeProvider) {
         templateUrl: 'templates/faq.html',
         controller: 'HomeController'
     })
+    .when('/aboutxt', {
+        templateUrl: 'templates/aboutxt.html',
+        controller: 'HomeController'
+    })
+    .when('/aboutxt/#tq', {
+        templateUrl: 'templates/aboutxt.html/#tq',
+        controller: 'HomeController'
+    })
+    .when('/homext/#ww', {
+        templateUrl: 'templates/homext.html/#ww',
+        controller: 'HomeController'
+    })
+    .when('/aboutxt/#ds', {
+        templateUrl: 'templates/aboutxt.html/#ds',
+        controller: 'HomeController'
+    })
+    .when('/homext/#up', {
+        templateUrl: 'templates/homext.html/#up',
+        controller: 'HomeController'
+    })
+    .when('/homext/#br', {
+        templateUrl: 'templates/homext.html/#br',
+        controller: 'HomeController'
+    })
+    .when('/homext', {
+        templateUrl: 'templates/homext.html',
+        controller: 'HomeController'
+    })
     .when('/fees', {
         templateUrl: 'templates/fees.html',
         controller: 'HomeController'
     })
+    .otherwise({
+        redirectTo: '/',
+        controller: 'HomeController'
+        })
 });
